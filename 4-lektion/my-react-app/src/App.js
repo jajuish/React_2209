b02343304a6a4842e75e51d4775b4f000757bd56
Ishita J <ishijaju@gmail.com>
2022-09-06 18:35:50 +0200

    Add react app skeleton

---
+++ 4-lektion/my-react-app/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import { Image } from "./components"
+
+function App() {
+  const a = 2;
+
+  return (
+    <div className="App">
+
+      <Image />
+
+    </div>
+  );
+}
+
+export default App;
